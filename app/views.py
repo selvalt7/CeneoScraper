c47b5c492151ff94c67bd4dbeacf159abf4ea489
selvalt7 <52136675+selvalt7@users.noreply.github.com>
2025-04-29 11:14:02 +0200

    Add project description and product_id form

--- app/views.py
+++ app/views.py
@@ -1,12 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/extract')
-def extract():
+@app.route('/display_form', methods=['POST'])
+def extract_post():
+  product_id = request.form.get('product_id')
+  return redirect(url_for('product', product_id=product_id))
+
+@app.route('/display_form', methods=['GET'])
+def display_form():
   return render_template('extract.html')
 
 @app.route('/products')
